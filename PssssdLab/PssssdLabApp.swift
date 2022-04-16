1012dc60260803a28f24d2efe6f871bd277ad39d
Umayanga Alahakoon <umayanga@usa.com>
2022-04-16 09:05:17 +0530

    Initial Commit

---
+++ PssssdLab/PssssdLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PssssdLabApp.swift
+//  PssssdLab
+//
+//  Created by Umayanga Alahakoon on 2022-04-16.
+//
+
+import SwiftUI
+
+@main
+struct PssssdLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
